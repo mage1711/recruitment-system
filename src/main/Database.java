71e5385c881de1bd2cc3f7ed892a84e0d1c6dc59
Islam Mohamed <islammohamedd1@gmail.com>
2020-05-08 06:22:03 +0200

    updated database

--- src/main/Database.java
+++ src/main/Database.java
@@ -4,6 +4,7 @@ package main;
 import java.sql.*;
 
 public class Database {
+    private static final String host = "jdbc:mysql://islamelbanna.info:3306/";
     private static final String user = "islamelb_recruitement_system";
     private static final String password = "FydL5LYELVYvHK7";
     private static final String name = "islamelb_recruitement_system";
@@ -14,7 +15,7 @@ public class Database {
 
     public static void init() {
         try {
-            myConn = DriverManager.getConnection("jdbc:mysql://islamelbanna.info:3306/" + name + params, user, password);
+            myConn = DriverManager.getConnection(host + name + params, user, password);
         } catch (Exception exc) {
             Database.error = exc.toString();
         }
